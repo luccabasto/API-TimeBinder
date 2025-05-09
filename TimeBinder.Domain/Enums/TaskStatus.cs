647eb35c428c218b678ae6ef4d366e23024a14e9
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2025-05-09 15:04:14 -0300

    feat: adicionando core enums para api

---
+++ TimeBinder.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeBinder.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        NaoIniciado,
+        EmAndamento,
+        Pausado,
+       // Interrompido, validar se faz sentido
+        Concluido,
+    }
+}
