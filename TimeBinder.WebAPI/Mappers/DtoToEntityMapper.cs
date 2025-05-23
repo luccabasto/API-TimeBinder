2f9fd75cb394010300740e2afe90907955c05d07
Lucas Basto <136653403+luccabasto@users.noreply.github.com>
2025-05-23 15:48:07 -0300

    feat/mappers in webapi

--- TimeBinder.WebAPI/Mappers/DtoToEntityMapper.cs
+++ TimeBinder.WebAPI/Mappers/DtoToEntityMapper.cs
@@ -1,6 +1,17 @@
-﻿namespace TimeBinder.WebAPI.Mappers
+﻿using TimeBinder.Domain.Entities;
+using TimeBinder.WebAPI.DTOs.Request;
+
+namespace TimeBinder.WebAPI.Mappers
 {
-    public class DtoToEntityMapper
+    public static class DtoToEntityMapper
     {
+        public static ToDo ToDomain(this CreateToDoRequest dto) =>
+            new ToDo(dto.Title, dto.Description);
+
+        public static Domain.Entities.Tarefa ToDomain(this CreateTarefaRequest dto, Guid toDoId) =>
+            Domain.Entities.Tarefa.Criar(dto.Title, dto.Description, toDoId);
+
+        public static Pomodoro ToDomain(this CreatePomodoroRequest dto) =>
+            Pomodoro.Create(dto.Description, dto.FocusDuration, dto.BreakDuration, dto.TotalCycles, dto.ToDoId, dto.TaskId);
     }
 }
